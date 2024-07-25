8a5374e65cba432f55019cca6ad28520aa213fad
Suhail Alvi <alvisuhail3@.com>
2024-07-25 14:24:37 +0530

    add auth

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">This is Next js Project we&apos;are coming</h1>
-      <h1 className="h2-bold">This is Next js Project hay hi </h1>
-      <h1 className="h3-bold">This is Next js Project</h1>
-    </div>
-  );
-}
